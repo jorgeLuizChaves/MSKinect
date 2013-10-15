8656ad7faf170fe6484509b7dec839bd73e733af
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2013-10-15 17:31:01 -0300

    first commit AuxiliarKinect project

---
+++ AuxiliarKinect/AuxiliarKinect/FuncoesBasicas/InicializadorKinect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace AuxiliarKinect.FuncoesBasicas
+{
+    public class InicializadorKinect
+    {
+        public static KinectSensor InicializarPrimeiroSensor(int anguloInicial)
+        {
+            KinectSensor kinectSensor = KinectSensor.KinectSensors.First(sensor => sensor.Status == KinectStatus.Connected);
+            kinectSensor.Start();
+            kinectSensor.ElevationAngle = anguloInicial;
+            return kinectSensor;
+        }
+    }
+}
