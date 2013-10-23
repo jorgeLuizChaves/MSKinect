ec5331413a02f8bf937d2f06fd63dbc979c87de7
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2013-10-23 14:02:15 -0200

    chapter 7 implement recognation moves part01 in auxiliarKinect project

---
+++ Chapter7/AuxiliarKinect/AuxiliarKinect/Movimentos/Poses/PoseT.cs
@@ -0,0 +1,78 @@
+﻿using AuxiliarKinect.FuncoesBasicas;
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuxiliarKinect.Movimentos.Poses
+{
+    public class PoseT : Pose
+    {
+        public int PercentualProgresso
+        {
+            get
+            {
+                return ContadorQuadros * 100 / QuadroIdentificacao;
+            }
+        }
+
+        public PoseT()
+        {
+            this.Nome = "Pose T";
+            this.QuadroIdentificacao = 10;
+        }
+
+        protected override bool PosicaoValida(Skeleton esqueletoUsuario)
+        {
+            Joint centroOmbros = esqueletoUsuario.Joints[JointType.ShoulderCenter];
+            Joint maoDireita = esqueletoUsuario.Joints[JointType.HandRight];
+            Joint cotoveloDireito = esqueletoUsuario.Joints[JointType.ElbowRight];
+            Joint maoEsquerda = esqueletoUsuario.Joints[JointType.HandLeft];
+            Joint cotoveloEsquerdo = esqueletoUsuario.Joints[JointType.ElbowLeft];
+            
+            double margemErro = 0.30;
+
+            bool maoDireitaAlturaCorreta =Util.CompararComMargemErro(margemErro, maoDireita.Position.Y, centroOmbros.Position.Y);
+            bool maoDireitaDistanciaCorreta = Util.CompararComMargemErro(margemErro, maoDireita.Position.Z, centroOmbros.Position.Z);
+            bool maoDireitaAposCotovelo = maoDireita.Position.X > cotoveloDireito.Position.X;
+
+            bool cotoveloDireitoAlturaCorreta = Util.CompararComMargemErro(margemErro, cotoveloDireito.Position.Y, centroOmbros.Position.Y);
+            bool cotoveloEsquerdoAlturaCorreta = Util.CompararComMargemErro(margemErro, cotoveloEsquerdo.Position.Y, centroOmbros.Position.Y);
+            bool maoEsquerdaAlturaCorreta = Util.CompararComMargemErro(margemErro, maoEsquerda.Position.Y, centroOmbros.Position.Y);
+            bool maoEsquerdaDistanciaCorreta = Util.CompararComMargemErro(margemErro, maoEsquerda.Position.Z, centroOmbros.Position.Z);
+            bool maoEsquerdaAposCotovelo = maoEsquerda.Position.X < cotoveloEsquerdo.Position.X;
+
+            return maoDireitaAlturaCorreta && maoDireitaDistanciaCorreta &&
+                    maoDireitaAposCotovelo && cotoveloDireitoAlturaCorreta &&
+                    maoEsquerdaAlturaCorreta && maoEsquerdaDistanciaCorreta &&
+                    maoEsquerdaAposCotovelo && cotoveloEsquerdoAlturaCorreta;
+        }
+
+        public override EstadoRastreamento Rastrear(Skeleton esqueletoUsuario)
+        {
+            EstadoRastreamento novoEstado;
+            if ( esqueletoUsuario != null && PosicaoValida(esqueletoUsuario))
+            {
+                if (QuadroIdentificacao == ContadorQuadros)
+                    novoEstado = EstadoRastreamento.IDENTIFICADO;
+                else
+                {
+                    novoEstado = EstadoRastreamento.EM_EXECUCAO;
+                    ContadorQuadros += 1;
+                }
+            }
+            else
+            {
+                novoEstado = EstadoRastreamento.NAO_IDENTIFICADO;
+                ContadorQuadros = 0;
+            }
+            return novoEstado;
+        }
+
+       
+
+
+    }
+}
